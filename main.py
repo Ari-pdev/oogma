d56626c3ac34498012562daf90ec304e230a4a1f
ari <communeofstars@gmail.com>
2024-10-17 19:46:29 +0530

    make webview start in seperate thread

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import webview
+import threading
+import asyncio
 import os
 
 CWD: str = os.getcwd()
 INDEX_HTML: str = "/index.html"
 
-js_func = {
-    'alert': 'alert("this is a test")'
-}
 
-
-def create_window():
+def start_webview():
     webview.create_window('Test Build', 'file://' + CWD + INDEX_HTML)
     webview.start()
 
 
 def main():
-    create_window()
+    webview_thread = threading.Thread(target=start_webview)
+    webview_thread.start()
 
 
 if __name__ == "__main__":
